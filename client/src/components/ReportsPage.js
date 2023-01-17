ca1cdb029b17f6d2fc17e17f9cafbc3b7044f04a
PRATEEKA001 <93429226+PRATEEKA001@users.noreply.github.com>
2023-01-17 10:17:08 +0800

    Create ReportsPage.js

---
+++ client/src/components/ReportsPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function ReportsPage(){
+    return(
+        <section>
+            <div class="container-fluid">
+            <h1>Reports Page</h1>
+            </div>
+        </section>
+      
+        <section>
+            <div class="container-fluid">
+            <h1>Sales by Product Type</h1>
+            </div>
+        </section>
+      
+        <section>
+            <div class="container-fluid">
+            <h1>Sales by Date</h1>
+            </div>
+        </section>
+      
+       <section>
+            <div class="container-fluid">
+            <h1>Sales by Promotion</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Dashboard;
